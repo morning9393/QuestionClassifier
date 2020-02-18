93e73987e287471fea53041b8bced769b35db859
morning9393 <243549184@qq.com>
2020-02-18 16:50:18 +0000

    implement bag of words

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,22 +1,6 @@
 import re
 import numpy as np
 
-stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd",
-              'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers',
-              'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
-              'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been',
-              'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but',
-              'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against',
-              'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down',
-              'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there',
-              'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no',
-              'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don',
-              "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't",
-              'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven',
-              "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan',
-              "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn',
-              "wouldn't", ",", "?", ".", "'s", "'t", "n't"]
-
 np.random.seed(16)
 with open('../data/dataset', 'r') as dataset:
     data = dataset.read()
@@ -36,6 +20,9 @@ with open('../data/corpus', 'w') as corpus:
     train_data = re.sub(r'\w+:\w+\s', '', train_data)
     train_data = train_data.lower()
     corpus.write(train_data)
+
+with open('../data/stop_words', 'r') as stop:
+    stop_words = stop.read().split()
 
 with open('../data/dictionary', 'w') as dictionary:
     train_list = train_data.split()
