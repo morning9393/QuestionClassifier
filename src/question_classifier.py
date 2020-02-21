f9611457b39e71bfca476086b87b8b01533da113
morning9393 <243549184@qq.com>
2020-02-21 02:03:09 +0000

    add DataLoader, improve accuracy = 35%

--- src/question_classifier.py
+++ src/question_classifier.py
@@ -1,5 +1,6 @@
 import re
 import torch
+from torch.utils.data import Dataset, DataLoader
 import random
 import numpy as np
 
@@ -12,26 +13,57 @@ def setup_seed(seed):
     torch.backends.cudnn.deterministic = True
 
 
-def load_sentences():
-    with open('../data/train_sentence_rep', 'r') as sentences:
+def load_questions(path):
+    with open(path, 'r') as questions:
         reps = []
-        for line in sentences:
+        for line in questions:
             pattern = re.compile(r'\w+:\w+\s')
             label = pattern.search(line).group().strip()
             vector = pattern.sub('', line, 1).split()
-            vector = [float(x) for x in vector]
-            reps.append((label, torch.tensor([vector], requires_grad=True)))
+            vector = [float(element) for element in vector]
+            reps.append((label, vector))
         return reps
 
 
 def load_labels():
     with open('../data/labels', 'r') as labels:
-        label_map = {}
-        target = 0
-        for line in labels:
-            label_map[line.strip()] = torch.tensor([target], dtype=torch.long)
-            target += 1
-        return label_map
+        return labels.read().split('\n')
+        # full_labels = labels.read().split('\n')
+        # super_label_set = set([])
+        # for label in full_labels:
+        #     super_label = to_super_label(label)
+        #     super_label_set.add(super_label)
+        # supper_label_list = list(super_label_set)
+        # supper_label_list.sort()
+        # return supper_label_list
+
+
+def to_super_label(label):
+    return label.split(':')[0]
+
+
+def get_data_loader(data_path, batch_size):
+    questions = QuestionSet(data_path)
+    loader = DataLoader(questions, batch_size=batch_size, shuffle=True)
+    return loader
+
+
+class QuestionSet(Dataset):
+
+    def __init__(self, data_path):
+        self.questions = load_questions(data_path)
+        self.labels = load_labels()
+
+    def __getitem__(self, index):
+        question = self.questions[index][1]
+        question = torch.tensor(question, requires_grad=True)
+        label = self.labels.index(self.questions[index][0])
+        # label = self.questions[index][0]
+        # label = self.labels.index(to_super_label(label))
+        return label, question
+
+    def __len__(self):
+        return len(self.questions)
 
 
 class Net(torch.nn.Module):
@@ -42,50 +74,55 @@ class Net(torch.nn.Module):
         self.fc2 = torch.nn.Linear(50, 50)
 
     def forward(self, x):
-        hidden = self.fc1(x)
-        relu = torch.nn.functional.relu(hidden)
-        out = self.fc2(relu)
+        out = self.fc1(x)
+        out = torch.nn.functional.relu(out)
+        out = self.fc2(out)
         predicted = torch.nn.functional.softmax(out, dim=1)
         return predicted
 
 
 setup_seed(16)
-reps = load_sentences()
-targets = load_labels()
+TRAIN_REP_PATH = '../data/train_sentence_rep'
+DEV_REP_PATH = '../data/dev_sentence_rep'
 
 net = Net()
 criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.1)
+optimizer = torch.optim.SGD(net.parameters(), lr=1)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)
+dataLoader = get_data_loader(TRAIN_REP_PATH, batch_size=1)
 
-net.train()
-for e in range(0, 2):
-    for t in range(0, len(reps)):
+for e in range(0, 100):
+    for t, (cla, train) in enumerate(dataLoader):
         optimizer.zero_grad()
-
-        x = reps[t][1]
-        y = targets[reps[t][0]]
-        y_pred = net(x)
-        loss = criterion(y_pred, y)
-        # print(loss)
-
+        cla_pred = net(train)
+        loss = criterion(cla_pred, cla)
         loss.backward()
         optimizer.step()
     scheduler.step()
     print('%d epoch finish' % (e + 1))
 torch.save(net, '../data/model.bin')
 
-net.eval()
+# --------------- train set test----------------
+trainDataLoader = get_data_loader(TRAIN_REP_PATH, batch_size=1)
 acc = 0
-for t in range(0, len(reps)):
-    x = reps[t][1]
-    y = targets[reps[t][0]]
-    output = net(x)
+for t, (cla, test) in enumerate(trainDataLoader):
+    output = net(test)
     _, pred = torch.max(output.data, 1)
-    print('y: %s, pred: %s ' % (y, pred))
-    if y == pred:
+    if cla == pred:
         acc += 1
-print('acc: ' + str(acc))
-acc_rate = float(acc) / float(len(reps))
-print('acc_rate: ' + str(acc_rate))
+print('train set acc: ' + str(acc))
+acc_rate = float(acc) / float(trainDataLoader.__len__())
+print('train set acc_rate: ' + str(acc_rate))
 
+# --------------- dev set test----------------
+devDataLoader = get_data_loader(DEV_REP_PATH, batch_size=1)
+acc = 0
+for t, (cla, test) in enumerate(devDataLoader):
+    output = net(test)
+    _, pred = torch.max(output.data, 1)
+    print('y: %s, pred: %s ' % (cla, pred))
+    if cla == pred:
+        acc += 1
+print('dev set acc: ' + str(acc))
+acc_rate = float(acc) / float(devDataLoader.__len__())
+print('dev set acc_rate: ' + str(acc_rate))
