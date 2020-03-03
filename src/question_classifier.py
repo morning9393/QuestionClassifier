fcf0c799ae0ac23d225572132f70567c22dbe7cb
morning9393 <243549184@qq.com>
2020-03-03 03:29:15 +0000

    add config files

---
+++ src/question_classifier.py
@@ -0,0 +1,80 @@
+import sys
+import torch
+import random
+import numpy as np
+import ensemble as en
+import model as md
+
+
+def main(argv):
+    if len(argv) < 4:
+        print('question_classifier.py [train|test] -config [configuration_file_path]')
+        sys.exit(2)
+    task = argv[1]
+    path = argv[3]
+    if argv[2] != '-config' or task not in ('train', 'test'):
+        print('question_classifier.py [train|test] -config [configuration_file_path]')
+        sys.exit(2)
+    config = load_config(path)
+    if config['model'] not in ['bow', 'bilstm', 'hybrid-cat', 'hybrid-add', 'cnn']:
+        print('model should in [bow|bilstm|hybrid-cat|hybrid-add|cnn]')
+    if task == 'train':
+        train(config)
+    else:
+        test(config)
+
+
+def load_config(path):
+    with open(path, 'r') as config_file:
+        config = {}
+        for line in config_file:
+            key, value = line.split('=')
+            config[key.strip()] = value.strip()
+        config['ensemble_size'] = int(config['ensemble_size'])
+        config['embedding_dim'] = int(config['embedding_dim'])
+        config['lstm_hidden'] = int(config['lstm_hidden'])
+        config['fc_input'] = int(config['fc_input'])
+        config['fc_hidden'] = int(config['fc_hidden'])
+        config['epoch'] = int(config['epoch'])
+        config['learning_rate'] = float(config['learning_rate'])
+        config['freeze'] = config['freeze'] == str(True)
+        return config
+
+
+def train(config):
+    classifier = en.QuestionClassifier(config['ensemble_size'], config['train_path'], config['vocabulary_path'],
+                                       config['labels_path'], config['stop_words_path'], config['pre_train_path'])
+    classifier.train(config['model'], config['embedding_dim'], config['lstm_hidden'], config['fc_input'],
+                     config['fc_hidden'], config['epoch'], config['learning_rate'], config['freeze'],
+                     config['test_path'])
+    classifier.save(config['model_path'])
+
+
+def test(config):
+    classifier = en.QuestionClassifier(config['ensemble_size'], config['train_path'], config['vocabulary_path'],
+                                       config['labels_path'], config['stop_words_path'], config['pre_train_path'])
+    classifier.load(config['model_path'])
+    test_set = md.QuestionSet(config['test_path'], config['vocabulary_path'], config['labels_path'],
+                              config['stop_words_path'], config['pre_train_path'])
+    f = open(config['output_path'], 'w')
+    old = sys.stdout
+    sys.stdout = f
+    acc, acc_rate = classifier.test(test_set, is_cnn=(config['model'] == 'cnn'), print_detail=True)
+    print('model: %s, ensemble: %d, pre_train: %s, freeze: %s, test_path: %s, acc: %d, acc rate: %f' %
+          (config['model'], config['ensemble_size'], config['pre_train_path'], config['freeze'], config['test_path'],
+           acc, acc_rate))
+    sys.stdout = old
+    f.close()
+
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+
+if __name__ == "__main__":
+    setup_seed(16)
+    main(sys.argv)
