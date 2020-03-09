c542b26f5353fbc9b659cdd737d56974510ccf1a
morning9393 <243549184@qq.com>
2020-03-09 16:42:09 +0000

    fix bug

--- src/experiments.py
+++ src/experiments.py
@@ -49,14 +49,14 @@ def ex1():
               ([1, TRAIN_PATH, VOCABULARY_PATH, LABELS_PATH, STOP_WORDS_PATH, PRE_TRAIN_PATH, K],
                ['bilstm', EMBEDDING_DIM, LSTM_HIDDEN, FC_INPUT, FC_HIDDEN, EPOCHS, LEARNING_RATE, FREEZE, TEST_PATH]),
               ([1, TRAIN_PATH, VOCABULARY_PATH, LABELS_PATH, STOP_WORDS_PATH, PRE_TRAIN_PATH, K],
-               ['hybrid-cat', EMBEDDING_DIM, LSTM_HIDDEN, 600, FC_HIDDEN, EPOCHS, LEARNING_RATE, FREEZE, TEST_PATH]),
+               ['hybrid-cat', EMBEDDING_DIM, LSTM_HIDDEN, 400, FC_HIDDEN, EPOCHS, LEARNING_RATE, FREEZE, TEST_PATH]),
               ([1, TRAIN_PATH, VOCABULARY_PATH, LABELS_PATH, STOP_WORDS_PATH, PRE_TRAIN_PATH, K],
                ['hybrid-add', EMBEDDING_DIM, LSTM_HIDDEN, FC_INPUT, FC_HIDDEN, EPOCHS, LEARNING_RATE, FREEZE,
                 TEST_PATH]),
               ([1, TRAIN_PATH, VOCABULARY_PATH, LABELS_PATH, STOP_WORDS_PATH, PRE_TRAIN_PATH, K],
-               ['cnn', EMBEDDING_DIM, LSTM_HIDDEN, 1184, FC_HIDDEN, EPOCHS, LEARNING_RATE, FREEZE, TEST_PATH]),
+               ['cnn', EMBEDDING_DIM, LSTM_HIDDEN, 784, FC_HIDDEN, EPOCHS, LEARNING_RATE, FREEZE, TEST_PATH]),
               ([ENSEMBLE_SIZE, TRAIN_PATH, VOCABULARY_PATH, LABELS_PATH, STOP_WORDS_PATH, PRE_TRAIN_PATH, K],
-               ['hybrid-cat', EMBEDDING_DIM, LSTM_HIDDEN, 600, FC_HIDDEN, EPOCHS, LEARNING_RATE, FREEZE, TEST_PATH])
+               ['hybrid-cat', EMBEDDING_DIM, LSTM_HIDDEN, 400, FC_HIDDEN, EPOCHS, LEARNING_RATE, FREEZE, TEST_PATH])
               ]
     for param in params:
         clf = en.QuestionClassifier(param[0][0], param[0][1], param[0][2], param[0][3], param[0][4], param[0][5],
@@ -109,14 +109,14 @@ def ex3():
               ([1, TRAIN_PATH, VOCABULARY_PATH, LABELS_PATH, STOP_WORDS_PATH, PRE_TRAIN_PATH, K],
                ['bilstm', EMBEDDING_DIM, LSTM_HIDDEN, FC_INPUT, FC_HIDDEN, EPOCHS, LEARNING_RATE, FREEZE, TEST_PATH]),
               ([1, TRAIN_PATH, VOCABULARY_PATH, LABELS_PATH, STOP_WORDS_PATH, PRE_TRAIN_PATH, K],
-               ['hybrid-cat', EMBEDDING_DIM, LSTM_HIDDEN, 600, FC_HIDDEN, EPOCHS, LEARNING_RATE, FREEZE, TEST_PATH]),
+               ['hybrid-cat', EMBEDDING_DIM, LSTM_HIDDEN, 400, FC_HIDDEN, EPOCHS, LEARNING_RATE, FREEZE, TEST_PATH]),
               ([1, TRAIN_PATH, VOCABULARY_PATH, LABELS_PATH, STOP_WORDS_PATH, PRE_TRAIN_PATH, K],
                ['hybrid-add', EMBEDDING_DIM, LSTM_HIDDEN, FC_INPUT, FC_HIDDEN, EPOCHS, LEARNING_RATE, FREEZE,
                 TEST_PATH]),
               ([1, TRAIN_PATH, VOCABULARY_PATH, LABELS_PATH, STOP_WORDS_PATH, PRE_TRAIN_PATH, K],
-               ['cnn', EMBEDDING_DIM, LSTM_HIDDEN, 1184, FC_HIDDEN, EPOCHS, LEARNING_RATE, FREEZE, TEST_PATH]),
+               ['cnn', EMBEDDING_DIM, LSTM_HIDDEN, 784, FC_HIDDEN, EPOCHS, LEARNING_RATE, FREEZE, TEST_PATH]),
               ([ENSEMBLE_SIZE, TRAIN_PATH, VOCABULARY_PATH, LABELS_PATH, STOP_WORDS_PATH, PRE_TRAIN_PATH, K],
-               ['hybrid-cat', EMBEDDING_DIM, LSTM_HIDDEN, 600, FC_HIDDEN, EPOCHS, LEARNING_RATE, FREEZE, TEST_PATH])
+               ['hybrid-cat', EMBEDDING_DIM, LSTM_HIDDEN, 400, FC_HIDDEN, EPOCHS, LEARNING_RATE, FREEZE, TEST_PATH])
               ]
     train_paths = ['../data/train.1000.txt', '../data/train.2000.txt', '../data/train.3000.txt',
                    '../data/train.4000.txt', '../data/train.5000.txt']
@@ -140,7 +140,7 @@ def ex4():
     print('************ ex4 begin %s ************' % begin_time)
     test_set = md.QuestionSet(TEST_PATH, VOCABULARY_PATH, LABELS_PATH, STOP_WORDS_PATH, PRE_TRAIN_PATH, K)
     params = [([ENSEMBLE_SIZE, TRAIN_PATH, VOCABULARY_PATH, LABELS_PATH, STOP_WORDS_PATH, PRE_TRAIN_PATH, K],
-               ['hybrid-cat', EMBEDDING_DIM, LSTM_HIDDEN, 600, FC_HIDDEN, EPOCHS, LEARNING_RATE, FREEZE, TEST_PATH])
+               ['hybrid-cat', EMBEDDING_DIM, LSTM_HIDDEN, 400, FC_HIDDEN, EPOCHS, LEARNING_RATE, FREEZE, TEST_PATH])
               ]
     for param in params:
         clf = en.QuestionClassifier(param[0][0], param[0][1], param[0][2], param[0][3], param[0][4], param[0][5],
@@ -152,7 +152,7 @@ def ex4():
               (param[1][0], param[0][0], param[0][5], param[1][7], acc, acc_rate))
         print('---------------------------------------------------------------------------------')
     finish_time = time.asctime(time.localtime(time.time()))
-    print('************ ex4 finish %s ************' % finish_time)
+    print('************ ex4 finish %s ************\n\n\n\n' % finish_time)
 
 
 def ex5():
@@ -178,7 +178,7 @@ def ex5():
                   param[1][7], param[1][8])
         print('---------------------------------------------------------------------------------')
     finish_time = time.asctime(time.localtime(time.time()))
-    print('************ ex5 finish %s ************\n\n\n\n' % finish_time)
+    print('************ ex5 finish %s ************' % finish_time)
 
 
 if __name__ == "__main__":
